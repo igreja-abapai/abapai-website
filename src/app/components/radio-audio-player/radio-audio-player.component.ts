637f5aed8efcdab84a5b6502f82b15817fb72942
Rayandson <rayandson_silva@hotmail.com>
2024-12-21 22:53:02 -0300

    feat: add tailwind css

---
+++ src/app/components/radio-audio-player/radio-audio-player.component.ts
@@ -0,0 +1,93 @@
+import { isPlatformBrowser } from '@angular/common';
+import {
+    Component,
+    ElementRef,
+    inject,
+    OnDestroy,
+    OnInit,
+    PLATFORM_ID,
+    ViewChild,
+} from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+    faPlay,
+    faBackwardStep,
+    faForwardStep,
+    faPause,
+    faRadio,
+} from '@fortawesome/free-solid-svg-icons';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+    selector: 'app-radio-audio-player',
+    standalone: true,
+    imports: [FontAwesomeModule],
+    templateUrl: './radio-audio-player.component.html',
+    styleUrl: './radio-audio-player.component.scss',
+})
+export class RadioAudioPlayerComponent implements OnDestroy {
+    @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
+
+    private platformId = inject(PLATFORM_ID);
+
+    faPlay = faPlay;
+    faBackwardStep = faBackwardStep;
+    faForwardStep = faForwardStep;
+    faPause = faPause;
+    faRadio = faRadio;
+
+    isPlaying = false;
+    volume = 100;
+    playingNow = '';
+    private updateSubscription?: Subscription;
+
+    ngOnDestroy(): void {
+        this.stopPlayingNowUpdates();
+    }
+
+    private startPlayingNowUpdates(): void {
+        if (isPlatformBrowser(this.platformId)) {
+            this.fetchPlayingNow();
+            this.updateSubscription = interval(20000).subscribe(() => {
+                this.fetchPlayingNow();
+            });
+        }
+    }
+
+    private stopPlayingNowUpdates(): void {
+        if (this.updateSubscription) {
+            this.updateSubscription.unsubscribe();
+            this.updateSubscription = undefined;
+        }
+        this.playingNow = '';
+    }
+
+    fetchPlayingNow(): void {
+        fetch('https://websitenoar.net/last/cur.php?hts=hts01&porta=12044')
+            .then((response) => response.text())
+            .then((data) => (this.playingNow = data.split('||IMAGE||')[0]))
+            .catch((error) => console.error('Error:', error));
+    }
+
+    toggleAudio(): void {
+        if (!this.isPlaying) {
+            this.isPlaying = true;
+            this.audioPlayer.nativeElement.load();
+            this.audioPlayer.nativeElement?.play();
+            this.startPlayingNowUpdates();
+        } else {
+            this.isPlaying = false;
+            this.audioPlayer.nativeElement?.pause();
+            this.stopPlayingNowUpdates();
+        }
+    }
+
+    handleVolumeChange(event: Event): void {
+        const target = event.target as HTMLInputElement;
+        const newVolume = parseInt(target.value);
+        this.volume = newVolume;
+        if (this.audioPlayer.nativeElement) {
+            this.audioPlayer.nativeElement.volume = newVolume / 100;
+        }
+    }
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,59 +1,25 @@
-import { Component, OnInit, OnDestroy, inject, PLATFORM_ID } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faRadio } from '@fortawesome/free-solid-svg-icons';
 import { ElementRef } from '@angular/core';
-import { AudioPlayerComponent } from '../components/audio-player/audio-player.component';
-import { interval, Subscription } from 'rxjs';
-import { isPlatformBrowser } from '@angular/common';
+import { RadioAudioPlayerComponent } from '../components/radio-audio-player/radio-audio-player.component';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [FontAwesomeModule, AudioPlayerComponent],
+    imports: [FontAwesomeModule, RadioAudioPlayerComponent],
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.scss',
 })
-export class LayoutComponent implements OnInit, OnDestroy {
-    private platformId = inject(PLATFORM_ID);
-
+export class LayoutComponent {
     faFacebookF = faFacebookF;
     faInstagram = faInstagram;
     faYoutube = faYoutube;
-    faRadio = faRadio;
-
-    playingNow = '';
-    private updateSubscription?: Subscription;
 
     constructor() {}
 
-    ngOnInit(): void {
-        // Only run on client side
-        if (isPlatformBrowser(this.platformId)) {
-            // Initial fetch
-            this.fetchPlayingNow();
-
-            // Set up interval for periodic updates
-            this.updateSubscription = interval(20000).subscribe(() => {
-                this.fetchPlayingNow();
-            });
-        }
-    }
-
-    ngOnDestroy(): void {
-        if (this.updateSubscription) {
-            this.updateSubscription.unsubscribe();
-        }
-    }
-
     scrollToSection(ref: ElementRef): void {
         // Implement scrolling logic
-    }
-
-    fetchPlayingNow(): void {
-        fetch('https://websitenoar.net/last/cur.php?hts=hts01&porta=12044')
-            .then((response) => response.text())
-            .then((data) => (this.playingNow = data.split('||IMAGE||')[0]))
-            .catch((error) => console.error('Error:', error));
     }
 }
