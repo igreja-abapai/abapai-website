376d442f013084309a8da57d8901ec2159683eba
Rayandson <rayandson_silva@hotmail.com>
2026-03-17 14:05:24 -0300

    fix: use correct api url

--- src/app/shared/environments/environment.prod.ts
+++ src/app/shared/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     prod: true,
-    clientApiBaseUrl: 'http://localhost:4333',
-    // clientApiBaseUrl: 'https://abapai-api.vercel.app',
+    // clientApiBaseUrl: 'http://localhost:4333',
+    clientApiBaseUrl: 'https://abapai-api.vercel.app',
 };
